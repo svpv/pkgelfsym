e58cf661931d60e47b3d02cff1cfb46f872fb5ea
Alexey Tourbin <alexey.tourbin@gmail.com>
2021-05-18 01:25:33 +0300

    pkgrec.h: initial revision
    Let's design a new format for storing ELF symbols.  ELF symbols come
    in compressed records, each record corresponding to a package.  These
    records are stored in a single file.  Hence we should be able to read
    a record from a file and decompress most, but not all, of its data.
    All integers and all LZ4 chunks are decompressed when reading a record;
    but sym1 frenc blob won't be unpacked to full-length symbols.  That's
    because we don't even need to store all of them.  The symbols from sym1
    are identified by symhash, and we may only need to store only the "hi"
    part of symhash, whereas the "lo" part is implicitly stored in fp47map
    (see bad-proc.c).
    
    The records are preceded by the index, which is conceptually a list
    of pairs <pkg-basename, record-size>.  When the record-size is zero,
    we know that the package provides no symbols (so what we don't have
    to process it again only to find out that it provides no symbols).
    Otherwise, we can PkgRec_read() the record.

---
+++ pkgrec.h
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "platform.h"
+
+struct PkgRecHdr {
+    uint16_t flags; // compression flags
+    uint16_t n0files; // number of files - 1
+    uint16_t nsym0[2]; // topsym.list counts
+    uint32_t nsym1; // frenc-mini symbol count
+    uint32_t nsym; // total symbol count (with dups from all files)
+};
+
+struct PkgRec {
+    struct PkgRecHdr h;
+    uint16_t *sym0; // decoded sym0 integers
+    uint16_t *fsym; // file reference for each symbol
+    uint32_t *fname; // maps file references into slab
+    uint16_t *n0dup; // dup count for each unique symbol - 1
+    uchar *sym1frenc; // frenc-mini blob, LZ-decompressed
+    uint32_t sym1frencsize;
+};
+
+struct slab;
+
+struct PkgRec *PkgRec_read(FILE *fp, struct slab *slab, uint32_t pkgname, uint32_t reclen);
